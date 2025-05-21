30b3845722e505e3679bdf42ec2ebbd030954d5c
X3-netizen <x8w8z8x@gmail.com>
2025-05-21 04:41:51 +0000

    Initial commit: My portfolio (X3-netizen)

---
+++ script.js
@@ -0,0 +1,7 @@
+<button id="call-button">Call me</button>
+
+<script>
+document.getElementById('call-button').addEventListener('click', function() {
+    window.location.href = 'tel:+233541411429';
+});
+</script>
